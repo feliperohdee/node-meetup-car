ca1ee6cec906f328fa8732c159b0987a1e45e795
Felipe Rohde <feliperohdee@gmail.com>
2016-05-04 23:11:08 -0300

    do changes

--- __build__/iot.js
+++ __build__/iot.js
@@ -1,12 +1,14 @@
 "use strict";
 var gpio = require('rpi-gpio');
-gpio.setup(7, gpio.DIR_OUT);
-setTimeout(function () {
-    gpio.write(7, 1);
-}, 1500);
-setInterval(function () {
-    gpio.write(7, 1);
-    setTimeout(function () {
-        gpio.write(7, 1);
-    }, 500);
-}, 1000);
+var pin = 15;
+gpio.setup(pin, gpio.DIR_OUT, function () { return go(); });
+function go() {
+    setInterval(function () {
+        console.log('write');
+        gpio.write(pin, 1);
+        setTimeout(function () {
+            console.log('teardown');
+            gpio.write(pin, 0);
+        }, 500);
+    }, 1000);
+}
